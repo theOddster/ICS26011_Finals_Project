ad6be18f6d4f4c287eccb147bee3240f68a60395
Jeric <jeric292@gmail.com>
2021-11-22 22:04:04 +0800

    Initial commit

---
+++ app/src/main/java/com/ccj/ics26011_finals_project/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.ccj.ics26011_finals_project.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
