8ef0e018118a4fc841ff19ab6dd38f8a53db4c57
itian <christianlim220@gmail.com>
2021-11-22 22:27:40 +0800

    Lim

---
+++ app/src/main/java/com/ccj/ics26011_finals_project/RegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.ccj.ics26011_finals_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
