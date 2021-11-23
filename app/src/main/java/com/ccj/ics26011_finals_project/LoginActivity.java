edecf7e278ccbee51f57b646dc982c0b253d94bd
itian <christianlim220@gmail.com>
2021-11-23 12:30:10 +0800

    ayaw mag run login register tas may error sa RegistrationActivity.java yung reg_btn

--- app/src/main/java/com/ccj/ics26011_finals_project/LoginActivity.java
+++ app/src/main/java/com/ccj/ics26011_finals_project/LoginActivity.java
@@ -2,13 +2,41 @@ package com.ccj.ics26011_finals_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView signUp;
+    Button signIn;
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signIn = findViewById(R.id.login_btn);
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
+        signUp = findViewById(R.id.sign_up);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/ccj/ics26011_finals_project/RegistrationActivity.java
+++ app/src/main/java/com/ccj/ics26011_finals_project/RegistrationActivity.java
@@ -2,13 +2,46 @@ package com.ccj.ics26011_finals_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    TextView signIn;
+    Button signUp;
+    EditText name, email, password;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        signIn = findViewById(R.id.sign_in);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email_reg);
+        password = findViewById(R.id.password_reg);
+        signUp = findViewById(R.id.reg_btn);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
+
 }
